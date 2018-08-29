665d6d483c6bd20bf9832b78f51f6721d0477a58
Peter A. Higgins <pahiggins@cenabo.com>
2018-08-29 14:01:24 +0100

    Add GET/POST/PUT/DELETE route handlers

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
+const Joi = require('joi');
 const express = require('express');
+
 const app = express();
+app.use(express.json());
+
+const courses = [
+    {
+        id: 1,
+        name: 'One'
+    },
+    {
+        id: 2,
+        name: 'Two'
+    },
+    {
+        id: 3,
+        name: 'Three'
+    }
+];
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
+
+app.get('/api/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(course => course.id === id);
+
+    if (!course) {
+        res.status(404).send('Course not found');
+    } else {
+        res.send(course);
+    }
+});
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(course => course.id === id);
+
+    if (!course) {
+        res.status(404).send('Course not found');
+        return;
+    }
+
+    const { error } = validateCourse(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(course => course.id === id);
+
+    if (!course) {
+        res.status(404).send('Course not found');
+        return;
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
